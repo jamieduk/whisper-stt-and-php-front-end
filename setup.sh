4f1a485645e35eb34b1c0f626cc9a42c793689fc
Jay <jamied_uk@hotmail.com>
2025-10-08 10:06:11 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ git clone https://github.com/ggml-org/whisper.cpp.git
 git clone https://github.com/jamieduk/whisper-stt-and-php-front-end.git
 sudo mv whisper-stt-and-php-front-end/{*,.*} whisper.cpp/ 2>/dev/null
 sudo rm -rf whisper-stt-and-php-front-end
-cd whisper.cpp.git
+cd whisper.cpp
 sudo chmod +x *.sh
 make
 echo "Setup Complete!"
